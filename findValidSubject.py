06485d5235357b63d4bb3c68b8163eba291a6839
David Welch <david.m.welch@gmail.com>
2012-12-17 16:31:37 -0600

    ENH: fixed some errors to find valid sessions

--- findValidSubject.py
+++ findValidSubject.py
@@ -5,21 +5,25 @@
 import fnmatch
 import os
 
-def findMatches(baseDir = '/paulsen/MRx', pattern = 'FMRI_HD_120/*/*'):
+def findMatches(baseDir = '/', pattern = '*', index = 5):
     sessions = set()
     for root, dirs, files in os.walk(baseDir):
         for directory in dirs:
-            if fnmatch.fnmatch(directory, pattern):
-                parts = directory.split(os.sep)
-                sessions.add(parts[4])
+            full = os.path.join(root, directory)
+            if fnmatch.fnmatch(full, pattern):
+                # print "Match: \t%s \n\t %s\n\n" %(full, pattern)
+                parts = full.split(os.sep)
+                # print parts[index]
+                sessions.add(parts[index])
     return sessions
 
 if __name__ == '__main__':
-    mrx = findMatches()
+    mrx = findMatches('/paulsen/MRx/FMRI_HD_120', '*/FMRI_HD_120/*/*/ANONRAW/FMRI_RestingStateConnectivity', 5)
     fs = findMatches('/paulsen/Experiments/20120722_JOY_DWI',
-                     'FMRI_HD_120/*/*/10_AUTO.NN3Tv20110419/*_FS/mri')
+                     '*/FMRI_HD_120/*/*/10_AUTO.NN3Tv20110419/*_FS/mri', 6)
     canidates = mrx.intersection(fs)
     post = findMatches('/paulsen/Experiments/20120801.SubjectOrganized_Results',
-                       'FMRI_HD_120/*/*/ACCUMULATED_POSTERIORS')
+                       '*/FMRI_HD_120/*/*/ACCUMULATED_POSTERIORS', 6)
     usable = canidates.intersection(post)
+    print "\n\n\nThis is the usable list: \n\n\n"
     print usable
