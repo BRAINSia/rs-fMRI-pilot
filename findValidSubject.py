d65fdf2e31c04a81f00907e357de6a4af6ae6f43
David Welch <david.m.welch@gmail.com>
2012-12-18 17:01:51 -0600

    ENH: Added temporary Matlab diary file for results analysis

--- findValidSubject.py
+++ findValidSubject.py
@@ -3,27 +3,35 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
 import fnmatch
+import glob
 import os
 
-def findMatches(baseDir = '/', pattern = '*', index = 5):
-    sessions = set()
-    for root, dirs, files in os.walk(baseDir):
-        for directory in dirs:
-            full = os.path.join(root, directory)
-            if fnmatch.fnmatch(full, pattern):
-                # print "Match: \t%s \n\t %s\n\n" %(full, pattern)
-                parts = full.split(os.sep)
-                # print parts[index]
-                sessions.add(parts[index])
-    return sessions
+
+def findMatches(filename, pattern = '*', index = 5):
+    with open(filename, 'w+') as fid:
+        for path in glob.iglob(pattern):
+            parts = path.split(os.sep)
+            fid.write('{0}\n'.format(parts[index]))
+    with open(filename, 'r') as fid2:
+        raw_sessions = fid2.readlines()
+    sessions = []
+    for item in raw_sessions:
+        sessions.append(str(item.rstrip('\n')))
+    return set(sessions)
+
 
 if __name__ == '__main__':
-    mrx = findMatches('/paulsen/MRx/FMRI_HD_120', '*/FMRI_HD_120/*/*/ANONRAW/FMRI_RestingStateConnectivity', 5)
-    fs = findMatches('/paulsen/Experiments/20120722_JOY_DWI',
-                     '*/FMRI_HD_120/*/*/10_AUTO.NN3Tv20110419/*_FS/mri', 6)
+    mrx = findMatches('dicom.txt', '/paulsen/MRx/FMRI_HD_120/*/*/ANONRAW/FMRI_RestingStateConnectivity', 5)
+    print "Found all DICOMs"
+    print mrx
+    fs = findMatches('freesurf.txt', '/paulsen/Experiments/20120722_JOY_DWI/FMRI_HD_120/*/*/10_AUTO.NN3Tv20110419/*_FS/mri', 6)
+    print "Found all brain.mgz"
+    print fs
     canidates = mrx.intersection(fs)
-    post = findMatches('/paulsen/Experiments/20120801.SubjectOrganized_Results',
-                       '*/FMRI_HD_120/*/*/ACCUMULATED_POSTERIORS', 6)
+    post = findMatches('posterior.txt',
+                       '/paulsen/Experiments/20120801.SubjectOrganized_Results/FMRI_HD_120/*/*/ACCUMULATED_POSTERIORS', 6)
+    print "Found all posteriors"
+    print post
     usable = canidates.intersection(post)
     print "\n\n\nThis is the usable list: \n\n\n"
     print usable
