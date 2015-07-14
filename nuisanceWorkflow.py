e27c0210a31ef3c03b0f3ae14859b32c9493d51b
David Welch <david.m.welch@gmail.com>
2015-07-14 16:34:44 -0500

    BUG: Replaced missing input file for CSF workflow EHN: Command line checking of --preprocess dependency for --maskGM and --maskWB HACK: Bug fix of Graphviz/pygraphviz plotting error when workflow begins with a number

--- nuisanceWorkflow.py
+++ nuisanceWorkflow.py
@@ -24,10 +24,10 @@ def maskNode(name, **inputkwargs):
     return node
 
 
-def csfWorkflow(input_file):
+def csfWorkflow():  #input_file):
     # Nodes
     warp = applyTransformNode(name='warpCSFtoFMRI', transform='nac2fmri')
-    mask = maskNode(name='csfMask', fileName='csfMask.nii', low=3, high=42, flags=['binary'], input_file=input_file)
+    mask = maskNode(name='csfMask', fileName='csfMask.nii', low=3, high=42, flags=['binary'])  #, input_file=input_file)
     avg = afninodes.maskavenode('AFNI_1D', 'afni3DmaskAve_csf')
     # Pipeline
     csf = pipe.Workflow(name='csf')
@@ -63,10 +63,9 @@ def gmWorkflow():
     return gm
 
 
-def wbWorkflow(input_mask):
+def wbWorkflow():
     # Nodes
     warp = applyTransformNode(name='warpBraintoFMRI', transform='nac2fmri')
-    warp.inputs.input_image = input_mask
     mask = maskNode(name='wholeBrainMask', fileName='wholeBrainMask.nii', low=0.5, high=1.0, flags=['largest'])
     avg = afninodes.maskavenode('AFNI_1D', name='afni3DmaskAve_whole')
     # Pipeline
@@ -81,7 +80,7 @@ def workflow(outputType, name="nuisance", **kwargs):
     # Nodes
     # in_fields = ["oned_file", "nactoFMRI", "t1toFMRI", "avg_file"]
     # inputnode = pipe.Node(interface=IdentityInterface(fields=in_fields), name="inputs")
-    csf_sub = csfWorkflow(kwargs['csf_input'])
+    csf_sub = csfWorkflow()  #kwargs['csf_input'])
     wm_sub = wmWorkflow()
 
     nuisance = pipe.Workflow(name=name)
@@ -92,7 +91,7 @@ def workflow(outputType, name="nuisance", **kwargs):
         deconvolve = afninodes.deconvolvenode(("Median_CSF", "Median_WM", "Median_GM"), "afni3Ddeconvolve")
         nuisance.connect(gm_sub, 'afni3DmaskAve_grm.out_file', deconvolve, 'stim_file_3')
     elif kwargs['maskwb']:
-        wb_sub = wbWorkflow(kwargs['wb_input'])  # Mask the whole brain
+        wb_sub = wbWorkflow()  # Mask the whole brain
         deconvolve = afninodes.deconvolvenode(("Median_CSF", "Median_WM", "Median_WholeBrain"), "afni3Ddeconvolve")
         nuisance.connect(wb_sub, 'afni3DmaskAve_whole.out_file', deconvolve, 'stim_file_3')
     else:
